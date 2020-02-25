35ea82e90fb84523a1801e035943105ed7497d94
Filippo Mailli <filippo.mailli@gmail.com>
2020-02-25 22:34:56 +0100

    .1 Creazione 16 numeri casuali no-repeat

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*
+- Generare 16 numeri casuali compresi tra 1 e 100 senza ripetizioni
+
+
+*/
+
+var numeroMinato;
+var numeriMinati = [];
+var i;
+var x = numeriMinati.length;
+var counter;
+
+for(i = 0; i < 16; i++){
+    counter = 0;
+    numeroMinato = Math.floor(Math.random() * 100) + 1;
+    for(x = i; x >= 0; x--) {
+        if(numeroMinato == numeriMinati[x]){
+            counter = counter + 1 ;
+        }
+        else{
+            conter = 0;
+        }
+    }
+    if(counter > 0) {
+        i--;
+    }
+    else{
+        numeriMinati[i] = numeroMinato;
+    }
+    x--;
+}
